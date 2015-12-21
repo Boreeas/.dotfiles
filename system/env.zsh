e3ab229c6e1f843d407dd84d669dab509412fe72
boreeas <malte.schuetze@fgms.de>
2015-12-21 23:26:17 +0100

    Add multirust installed crates to PATH

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,5 @@
 export EDITOR='emacs -nw'
 export RUST_SRC_PATH='/usr/local/src/rust/1.0.0/src'
+export MULTIRUST_HOME=~/.multirust
+export MULTIRUST_NIGHTLY=$MULTIRUST_HOME/toolchains/nightly
+export MULTIRUST_BIN=$MULTIRUST_NIGHTLY/bin
