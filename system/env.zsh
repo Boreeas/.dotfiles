e77d92641c47b6c9a0b09c61380200a6665bd937
boreeas <malte.schuetze@fgms.de>
2016-08-06 17:46:52 +0200

    Use cargo settings if existant

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,3 @@
 export EDITOR='emacs -nw'
 export RUST_SRC_PATH='/usr/local/src/rust/1.0.0/src'
-export MULTIRUST_HOME=~/.multirust
-export MULTIRUST_NIGHTLY=$MULTIRUST_HOME/toolchains/nightly
-export MULTIRUST_BIN=$MULTIRUST_NIGHTLY/bin
+[[ -d ~/.cargo ]] && source ~/.cargo/env
