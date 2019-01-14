6b89a9cb53766b4aebda0db2caaa1ff0b2f64da5
boreeas <malte.schuetze@fgms.de>
2019-01-14 22:09:38 +0100

    We don't need that, actually

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,2 @@
 export PATH="~/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
-export PATH="$PATH:/opt/php71/usr/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
