801ceb6c21d4024c2479385ace47c79d6706e3f6
boreeas <malte.schuetze@fgms.de>
2019-01-14 18:46:05 +0100

    Update path

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,2 +1,3 @@
 export PATH="~/bin:./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+export PATH="$PATH:/opt/php71/usr/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
