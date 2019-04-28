27421318fa7ec3deb33d7d416fe552ad31f8e33b
boreeas <malte.schuetze@fgms.de>
2019-04-28 14:33:14 +0200

    Update env

--- system/env.zsh
+++ system/env.zsh
@@ -1,4 +1,5 @@
-export EDITOR='emacs -nw'
+export DOWNLOAD_KEYSERVER='keys.gnupg.net'
+export EDITOR='vim'
 export RUST_SRC_PATH='/usr/local/src/rust/1.0.0/src'
 [[ -d ~/.cargo ]] && source ~/.cargo/env
 
